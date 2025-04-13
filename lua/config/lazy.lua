d11fb6567fd5f225c1d51ef6ad884f21af26bb50
Abhishek Jethuri <abhishek.jethuri@gmail.com>
2025-04-13 16:27:48 +0530

    Add prettier

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,9 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- Add LazyVim Extras
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
