d7c345ae56f84138742bfc9746e75542caa2bf2a
Abhishek Jethuri <abhishek.jethuri@gmail.com>
2025-04-14 10:00:28 +0530

    Remove unnecessary opts from snacks.nvim

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,20 +1,5 @@
 return {
   "folke/snacks.nvim",
-  opts = {
-    picker = {
-      sources = {
-        explorer = {
-          win = {
-            list = {
-              keys = {
-                ["<leader>/"] = false,
-              },
-            },
-          },
-        },
-      },
-    },
-  },
   keys = {
     { "<leader>/", false },
   },
