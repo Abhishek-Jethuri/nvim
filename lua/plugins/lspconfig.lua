5d2712704a7ecba8e821e19d6bb4f3c71bd085d3
Abhishek Jethuri <abhishek.jethuri@gmail.com>
2025-04-28 22:33:31 +0530

    Remove typescript server and prettier and instead install it from LazyExtras

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,19 +1,7 @@
--- add tsserver and setup with typescript.nvim instead of lspconfig
 return {
   "neovim/nvim-lspconfig",
-  dependencies = {
-    "jose-elias-alvarez/typescript.nvim",
-    init = function()
-      require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-        vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-      end)
-    end,
-  },
   ---@class PluginLspOpts
   opts = {
-    ---@type lspconfig.options
     servers = {
       clangd = {
         keys = {
@@ -50,24 +38,13 @@ return {
           clangdFileStatus = true,
         },
       },
-      -- tsserver will be automatically installed with mason and loaded with lspconfig
-      tsserver = {},
     },
-    -- you can do any additional lsp server setup here
-    -- return true if you don't want this server to be setup with lspconfig
-    ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
     setup = {
-      -- example to setup with typescript.nvim
-      tsserver = function(_, opts)
-        require("typescript").setup({ server = opts })
-        return true
       clangd = function(_, opts)
         local clangd_ext_opts = LazyVim.opts("clangd_extensions.nvim")
         require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
         return false
       end,
-      -- Specify * to use this function as a fallback for any server
-      -- ["*"] = function(server, opts) end,
     },
   },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,8 +2,6 @@ return {
   "williamboman/mason.nvim",
   opts = {
     ensure_installed = {
-      "typescript-language-server",
-      "prettierd",
     },
   },
 }
